beff31b857281f1d18748d8c7c2aa2d54c2541a6
Luiz0770 <ramosluizfelipe104@gmail.com>
2024-04-29 17:43:32 -0300

    finaliznado contato e criando destalhes dos wines

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//Formulario de contato
+const btnEnviarFormulario = document.getElementById("botao-contato")
+
+btnEnviarFormulario.addEventListener('click', () => {
+    alert("Mensagem enviado com sucesso!")
+})
+
+
+//Redirecionando para pagina de detalhes
+
+
+
